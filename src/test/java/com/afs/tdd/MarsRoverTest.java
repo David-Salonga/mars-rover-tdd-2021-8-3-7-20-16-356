af33cca7680c62980b1b00efa1fba3a9a32e7ef1
David Salonga <fdc.salonga@gmail.com>
2021-08-05 20:16:43 +0800

    [Mars Rover][Francis] test: added should return locationX 0 locationY -1  direction S given locationX 0 locationY0 direction S and S

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -48,6 +48,20 @@ class MarsRoverTest {
 
     }
 
+    @Test
+    void should_return_locationX_0_locationY_negative_1_direction_S_when_execute_command_given_locationX_0_locationY_0_direction_S_and_M() {
+        //given
+        MarsRover marsRover = new MarsRover(new RoverStatus(0,0,"S"));
+        RoverStatus expectedRoverStatus = new RoverStatus(0,-1,"S");
+        //when
+        marsRover.executeCommand("M");
+        //then
+        assertEquals(expectedRoverStatus.getLocationY(), marsRover.getRoverStatus().getLocationY());
+        assertEquals(expectedRoverStatus.getLocationX(), marsRover.getRoverStatus().getLocationX());
+        assertEquals(expectedRoverStatus.getDirection(), marsRover.getRoverStatus().getDirection());
+    }
+
+
 
     
 }
